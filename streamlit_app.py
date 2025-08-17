f7e958b2c8e7c2d25da015c5d00fcde1cc9141ea
Wafi FR <167063534+Wafi-FR@users.noreply.github.com>
2025-08-17 19:09:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 
@@ -87,4 +88,3 @@ if uploaded_file:
 
 else:
     st.info("Silakan upload file dataset untuk mulai analisis.")
-
